b4d2731f628e736f854565232720eee8b4c57b86
unknown <shilpag022@gmail.com>
2024-03-05 14:05:57 +0000

    added new changes6

--- products/views.py
+++ products/views.py
@@ -18,10 +18,12 @@ def list_products(request):
     page=1
     if request.GET:
         page=request.GET.get('page',1)  #pagination
-    product_list=Product.objects.all()
+    product_list=Product.objects.order_by('priority')
     product_paginator=Paginator(product_list,2)  #pagination
     product_list=product_paginator.get_page(page)
     context={'products':product_list}
     return render(request,'products.html',context)
-def detail_product(request):
-    return render(request,'product_detail.html')
+def detail_product(request,pk):
+    product=Product.objects.get(pk=pk)
+    context={'product':product}
+    return render(request,'product_detail.html',context)
