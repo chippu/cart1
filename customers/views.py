eb7c88137e298017d0229811c483b8782813b03a
unknown <shilpag022@gmail.com>
2024-03-08 12:38:39 +0000

    added new changes9

--- customers/views.py
+++ customers/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .models import Customer
+def sign_out(request):
+    logout(request)
+    return redirect('home')
 
 # Create your views here.
 def show_account(request):
+    context={}
     
     if request.POST and 'register' in request.POST:
+        context['register']=True
         try:
             username=request.POST.get('username')
             password=request.POST.get('password')
@@ -14,7 +20,7 @@ def show_account(request):
             address=request.POST.get('address')
             phone=request.POST.get('phone')
             # create user account
-            user=User.objects.create(
+            user=User.objects.create_user(
                 username=username,
                 password=password,
                 email=email
@@ -25,9 +31,26 @@ def show_account(request):
                 phone=phone,
                 address=address
                 )
-            return redirect('home')
+            # return redirect('home')
+            success_message="User registered successfully"
+            messages.success(request,success_message)
+            
         except Exception as e:
-            error_message="Duplicate username or invalid credential"
+            error_message="Duplicate username or invalid inputs"
             messages.error(request,error_message)
+    if request.POST and 'login' in request.POST:
+        context['register']=False
         
-    return render(request,'account.html')
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"invalid user credential")
+            
+                
+            
+        
+    return render(request,'account.html',context)
