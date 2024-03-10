5134e0b7fc4caac7573e5653440c900c69a2fdc6
unknown <shilpag022@gmail.com>
2024-03-10 19:26:56 +0000

    added new changes14

--- orders/admin.py
+++ orders/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from orders.models import Order,OrderedItem
 
 # Register your models here.
-admin.site.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_filter=[
+        "owner",
+        "order_status",
+        
+    ]
+    search_fields=(
+        "owner",
+        "id",
+    )
+
+admin.site.register(Order,OrderAdmin)
