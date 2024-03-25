c06a012d33030261982ad6691432d33ca27f6652
unknown <shilpag022@gmail.com>
2024-03-25 10:41:28 +0000

    added new changes15

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+
 urlpatterns = [
     path('',views.index,name='home'),
     path('product_list',views.list_products,name='list_product'),
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import Product
 from django.core.paginator import Paginator
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -34,3 +35,5 @@ def detail_product(request,pk):
     product=Product.objects.get(pk=pk)
     context={'product':product}
     return render(request,'product_detail.html',context)
+
+
